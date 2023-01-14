76a778b03e6e595bee04a77c6785e3b73ff20024
faheem-lab <117493535+faheem-lab@users.noreply.github.com>
2023-01-13 20:21:55 -0600

    added variable VMNAME

--- Jenkins/gce-create.groovy
+++ Jenkins/gce-create.groovy
@@ -29,7 +29,7 @@ pipeline {
 	stage('Create Instance') {
 	 steps {
     
-    sh 'gcloud compute instances create cello --zone=us-central1-a'
+    sh 'gcloud compute instances create $VMNAME --zone=us-central1-a'
         
     }
     }
